713aecd796fcae9bb9116f4fa17058a17c812fa8
Pedro Igor <pedroigor.silva@outlook.com>
2021-09-01 00:06:21 -0300

    Update note by note id

--- backend/note/views.py
+++ backend/note/views.py
@@ -4,7 +4,7 @@ from rest_framework.serializers import Serializer
 from rest_framework.views import APIView 
 from .serializers import NoteSerializer
 from rest_framework.response import Response
-from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.exceptions import NotFound
 from user.models import User
 from note.models import Note
 
@@ -27,8 +27,19 @@ class NoteView(APIView):
         user = User().get_user(request)
         notes = Note.objects.filter(user_id=user.id, id=note).all()
         if not notes:
-            raise AuthenticationFailed(f'Note {note} doesn\'t exist')
+            raise NotFound(f'Note {note} doesn\'t exist')
         serializer = NoteSerializer(data=notes, many=True)
         serializer.is_valid()
         notes.delete()
         return Response({'message': f'The note {note} was successfully deleted.'})
+    def patch(self, request, note):
+        user = User().get_user(request)
+        found_note = Note.objects.filter(user_id=user.id, id=note)
+        if not found_note:
+            raise NotFound(f'Note {note} doesn\'t exist')
+        data = dict(request.data.dict())
+        data.update({'user': user.id})
+        serializer = NoteSerializer(data = data)
+        serializer.is_valid(raise_exception=True)
+        found_note.update(title = data['title'], content = data['content'])
+        return Response(serializer.data)
