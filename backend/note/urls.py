400a2362c32a2c98200a845815dd1744a1015376
Pedro Igor <pedroigor.silva@outlook.com>
2021-08-31 23:31:54 -0300

    List notes by logged user

--- backend/note/urls.py
+++ backend/note/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CreateNoteView.as_view()),
+    path('', views.NoteView.as_view())
 ]
--- backend/note/views.py
+++ backend/note/views.py
@@ -1,16 +1,24 @@
 from django.http import HttpResponse
-from rest_framework.views import APIView
+from rest_framework import serializers
+from rest_framework.serializers import Serializer
+from rest_framework.views import APIView 
 from .serializers import NoteSerializer
 from rest_framework.response import Response
 from user.models import User
+from note.models import Note
 
-class CreateNoteView(APIView):
+class NoteView(APIView):
     def post(self, request):
         user = User().get_user(request)
-        print(user.id)
         data = dict(request.data.dict())
         data.update({'user': user.id})
         serializer = NoteSerializer(data = data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        return Response(serializer.data)
+    def get(self, request):
+        user = User().get_user(request)
+        notes = Note.objects.filter(user_id=user.id).all()
+        serializer = NoteSerializer(data=notes, many=True)
+        serializer.is_valid()
         return Response(serializer.data)
