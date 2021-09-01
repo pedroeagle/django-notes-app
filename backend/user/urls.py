a18ce42df2be9c7f3b898c4e8b8d1fbe3848b0fa
Pedro Igor <pedroigor.silva@outlook.com>
2021-08-31 21:40:52 -0300

    Add function to login user and return a jwt token

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, LoginView
 
 urlpatterns = [
     path('register', RegisterView.as_view()),
-    #path('login', views.login, name='login')
+    path('login', LoginView.as_view())
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,7 +1,13 @@
+from django import http
 from rest_framework import serializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
 from .serializers import UserSerializer
+from .models import User
+from dotenv import load_dotenv
+import os
+import jwt, datetime
 
 class RegisterView(APIView):
     def post(self, request):
@@ -9,3 +15,29 @@ class RegisterView(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+
+        user = User.objects.filter(username=username).first()
+
+        if user is None:
+            raise AuthenticationFailed('User or password is incorrect.')
+        if not user.check_password(password):
+            raise AuthenticationFailed('User or password is incorrect.')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow()+datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+        load_dotenv()
+        token = jwt.encode(payload, os.getenv('JWT_SECRET'), algorithm='HS256').decode('utf-8')
+        response = Response()
+        response.data = {
+            'jwt': token
+        }
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        return response
